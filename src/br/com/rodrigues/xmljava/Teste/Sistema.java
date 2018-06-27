ce6e649ddc8854214caa907e8a2715dbc0caa04c
Igor Rodrigues <igor.chagas.rodrigues@gmail.com>
2018-06-27 10:48:53 -0300

    Capturando dados do xml e Criando um objeto Produto

---
+++ src/br/com/rodrigues/xmljava/Teste/Sistema.java
@@ -0,0 +1,37 @@
+package br.com.rodrigues.xmljava.Teste;
+
+import java.io.IOException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import br.com.rodrigues.xmljava.Model.Produto;
+
+public class Sistema {
+	public static void main(String[] args) throws ParserConfigurationException, SAXException {
+		DocumentBuilderFactory fabrica = DocumentBuilderFactory.newInstance();
+		DocumentBuilder builder = fabrica.newDocumentBuilder();
+		try {
+			Document document = builder.parse("src/vendas.xml");
+			NodeList produtos = document.getElementsByTagName("produto");
+			for (int i = 0; i < produtos.getLength(); i++) {
+				Element produto = (Element) produtos.item(i);
+				String nome = produto.getElementsByTagName("nome").item(0).getTextContent();
+				double preco = Double.parseDouble(produto.getElementsByTagName("preco").item(0).getTextContent());
+				Produto prod = new Produto(nome, preco);
+				
+				System.out.println(prod);
+			}
+		} catch (IOException e) {
+			System.out.println("Inexiste Documento - Verifique o caminho");
+			e.printStackTrace();
+			;
+		}
+	}
+}
